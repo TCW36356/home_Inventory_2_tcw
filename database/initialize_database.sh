e62e188814c08b93ef01def6b3d6558d49530ed7
Tom Wilson <tomcwilson87@gmail.com>
2022-12-06 21:39:46 -0500

    trying to fix initialization script, adding foreign key to insert_test_data.

--- database/initialize_database.sh
+++ database/initialize_database.sh
@@ -19,14 +19,15 @@ echo $d': Creating database...' | tee -a logs/create_database.log
 mysql < dbfiles/create_database.sql 2>&1 | tee -a logs/create_database.log
 echo $d': Creating user...' | tee -a logs/create_user.log
 mysql < dbfiles/create_user.sql 2>&1 | tee -a logs/create_user.log
-echo $d': Creating tables...' | tee -a logs/create_tables.log
-mysql < dbfiles/create_tables.sql 2>&1 | tee -a logs/create_tables.log
-echo $d': Inserting test data...' | tee -a logs/insert_test_data.log
-mysql < dbfiles/insert_test_data.sql 2>&1 | tee -a logs/insert_test_data.log
-echo  $d': Adding inventories table...' | tee -a logs/create_table_inventories.log
+echo $d': Creating tables...' | tee -a logs/create_table_inventories.log
 mysql < dbfiles/create_table_inventories.sql 2>&1 | tee -a logs/create_table_inventories.log
 echo  $d': Inserting inventory row...' | tee -a logs/insert_inventory_row.log
 mysql < dbfiles/insert_inventory_row.sql 2>&1 | tee -a logs/insert_inventory_row.log
+echo  $d': Adding items table...' | tee -a logs/create_items_table.log
+mysql < dbfiles/create_items_table.sql 2>&1 | tee -a logs/create_items_table.log
+echo $d': Inserting test data...' | tee -a logs/insert_test_data.log
+mysql < dbfiles/insert_test_data.sql 2>&1 | tee -a logs/insert_test_data.log
+
 
 
 
